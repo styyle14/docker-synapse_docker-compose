4cc022680b4c4acab26ea7bc7f505501dbc7713b
styyle14 <styyle14@gmail.com>
2017-02-08 21:20:49 -0600

    add docker-compose files, run folder, docker-synapse submodule, .dockerignore and .gitignore files

---
+++ run/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+echo -n "Synapse Domain Name: "
+read SYNAPSE_DOMAIN_NAME
+
+if [ -z "$SYNAPSE_DOMAIN_NAME" ]; then
+	echo "No domain name entered."
+	echo "Exiting now."
+	exit 1
+fi
+
+python -m synapse.app.homeserver \
+    --server-name "$SYNAPSE_DOMAIN_NAME" \
+    --config-path homeserver.yaml \
+    --generate-config \
+    --report-stats=yes
+
