a0ecd330fbb9ffbdb30c53015aeb295fea393173
styyle14 <styyle14@gmail.com>
2017-02-11 17:40:55 -0600

    replace synctl with direct python call since this will be in the foreground

--- run/register-user.sh
+++ run/register-user.sh
@@ -5,6 +5,5 @@ set -e
 
 DOCKER_SYNAPSE_CONFIG_FILE="./config/homeserver.yaml"
 
-synctl start "$DOCKER_SYNAPSE_CONFIG_FILE"
 register_new_matrix_user -c "$DOCKER_SYNAPSE_CONFIG_FILE" "https://localhost:8448"
 
